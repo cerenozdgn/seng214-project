9bbbdda2e266e007333326d239896778bcf7f512
Nursu Baltaci <nursuubaltacii@gmail.com>
2023-05-21 18:58:19 +0300

    map eklendi

--- book-shop/js/map.js
+++ book-shop/js/map.js
@@ -1,18 +1,17 @@
 /* ----- Google Map ----- */
 if ($("#map").length) {
-    function initialize() {
-        var mapOptions = {
-            zoom: 16,
-            scrollwheel: false,
-            center: new google.maps.LatLng(40.712775, -74.005973) //please add your location here
-        };
-        var map = new google.maps.Map(document.getElementById('map'),
-            mapOptions);
-        var marker = new google.maps.Marker({
-            position: map.getCenter(),
-            //icon: 'images/locating.png', if u want custom
-            map: map
-        });
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
+  function initialize() {
+    var mapOptions = {
+      zoom: 16,
+      scrollwheel: false,
+      center: new google.maps.LatLng(39.92301054293743, 32.861869610238294), //please add your location here
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var marker = new google.maps.Marker({
+      position: map.getCenter(),
+      //icon: 'images/locating.png', if u want custom
+      map: map,
+    });
+  }
+  google.maps.event.addDomListener(window, "load", initialize);
 }
